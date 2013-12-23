18f3279eb20eaa33181ccb2d0e817fc2270b2662
Shaggy TwoDope <twodopeshaggy@gmail.com>
2013-12-22 19:58:03 -0800

    pae clear on complete and shutdown

--- paeinstaller.sh
+++ paeinstaller.sh
@@ -1,44 +1,29 @@
 #!/bin/bash
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
 # Script to check for pae capability, and then to install a pae kernel.
-# Author: John 'ShaggyTwoDope' Jenkins 
-# Credits: Jerry Bezencon The Cleaner and Editor , Stripe The Tester
+# Author: John 'ShaggyTwoDope' Jenkins
+# Contributor: Jerry Bezencon
 
 show_menu(){
-    MENU=`tput sgr0`
-    BOLD=`tput bold`
-    FGRED=`echo "\033[41m"`
-    RED_TEXT=`echo "\033[31m"`
-    ENTER_LINE=`echo "\033[33m"`
+    NORMAL=`echo "\033[m"`
+    MENU=`echo "\033[92m"` #Light Green
+    NUMBER=`echo "\033[97m"` #White
+    FGRED=`echo "\033[41m"` #Red
+    WHITE_TEXT=`echo "\033[97m"` #White
+    ENTER_LINE=`echo "\033[92m"` #Light Green
     CHECKPAE=$(
     if [ -z "(grep -w pae /proc/cpuinfo)" ]; then
-     echo "${RED_TEXT}Your Processor is NOT PAE capable, please press Enter to exit."; 
+     echo "${WHITE_TEXT}Your Processor is ${NUMBER}NOT PAE ${MENU}capable, please press Enter to exit."; 
                                 else
-     echo "${MENU}Your Processor is PAE capable, you can proceed with installation.";
+     echo "${MENU}Your Processor is ${NUMBER}PAE ${MENU}capable, you can procede with installation.${NORMAL}";
                               fi
       )
-
     echo -e "${CHECKPAE}"
-    echo -e "${BOLD}**************************************************"
-    echo -e "${BOLD}Linux Lite - PAE Kernel Installer"
-    echo -e "${BOLD}**************************************************"
-    echo -e "${MENU}** 1) Read More (This will launch the Help Manual)"
-    echo -e "${MENU}** 2) Install the PAE Kernel"
-    echo -e "${MENU}** 3) Reboot into the new kernel"
-    echo -e "${MENU}**************************************************"
-    echo -e "${MENU}Please choose a menu option then press Enter or press ${RED_TEXT}Enter now to exit.${MENU}"
+    echo -e "${MENU}**************************************************${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 1)${MENU} Read More (This will launch the Help Manual) ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 2)${MENU} Install the ${NUMBER}PAE ${MENU}Kernel ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 3)${MENU} Reboot into the new kernel ${NORMAL}"
+    echo -e "${MENU}**************************************************${NORMAL}"
+    echo -e "${ENTER_LINE}Please choose a menu option then press Enter or press ${WHITE_TEXT}Enter now to exit.${NORMAL}"
     read opt
 }
 
@@ -59,9 +44,12 @@ while [ opt != '' ]
         case $opt in
         1) clear;
         option_picked "Launched Browser with Help Manual";
-    firefox https://www.linuxliteos.com/manual/install.html#updates &
-    show_menu;
+    xdg-open /usr/share/doc/xfce4-utils/html/C/install.html#updates &
+    	clear;
+	show_menu;
         ;;
+
+
 
         2) clear;
             option_picked "Installation Selected";
@@ -70,12 +58,15 @@ while [ opt != '' ]
                        else
                           sudo apt-get install linux-generic-pae linux-headers-generic-pae -y
                                 fi
+
+            clear;
             show_menu;
             ;;
 
+
         3) clear;
             option_picked "Reboot Selected";
-            ls -art;
+            sudo shutdown -r now;
             show_menu;
             ;;
 
