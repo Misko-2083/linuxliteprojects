4241ac142632b754426900ba2847f50bfa2be1d5
Shaggy TwoDope <twodopeshaggy@gmail.com>
2013-12-29 22:35:51 -0800

    setup.py fiddling

--- liteupdater/setup.py
+++ liteupdater/setup.py
@@ -11,6 +11,9 @@ setup(name = "liteupdater",
     author = "John Jenkins",
     author_email = "twodopeshaggy@gmail.com",
     url = "http://linuxliteos.com",
+    description = ("An extremely simple update tool for Linux Lite, not built "
+                                           "for other distros yet."),
+    platform = "Linux"
     license = "GPL2",
     scripts = ['liteupdater','liteupdatertray'],
     data_files = [('share/liteupdater',['README','AUTHORS','COPYING',]),('/usr/share/pixmaps', ['updates_ll.png','updates_avail.png','menu-icon.png'])]    
