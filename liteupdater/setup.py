827e2d273140663f3bd7901ba327d90faa168816
Shaggy TwoDope <twodopeshaggy@gmail.com>
2013-12-29 17:33:21 -0800

    init version of updater and tray

---
+++ liteupdater/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#
+#       setup.py
+#       
+
+from distutils.core import setup
+
+setup(name = "liteupdater",
+    version = "0.1",
+    description = "update notifications",
+    author = "John Jenkins",
+    author_email = "twodopeshaggy@gmail.com",
+    license = "GPL2",
+    scripts = ['liteupdater','liteupdatertray'],
+    data_files = [('share/liteupdater',['README','AUTHORS','COPYING','trayicon.png','availabletrayicon.png'])]    
+) 
